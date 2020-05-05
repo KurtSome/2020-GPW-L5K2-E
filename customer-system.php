8a23a4cf3bce34fe646ee5aaa664404f3a35fc7a
KurtSome <45254780+KurtSome@users.noreply.github.com>
2020-05-05 17:31:17 +0800

    Trying to change styles.

--- customer-system.php
+++ customer-system.php
@@ -14,7 +14,7 @@ include('customer-system-header.html');
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Full Width Pics - Start Bootstrap Template</title>
+  <title>Chengdu Bus Company</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -30,7 +30,7 @@ include('customer-system-header.html');
 
 
   <!-- Header - set the background image for the header in the line below -->
-  <header class="py-5 bg-image-full" style="background-image: url('https://unsplash.it/1900/1080?image=1076');">
+  <header class="py-5 bg-image-full" style="background-image: url('https://image.baidu.com/search/detail?ct=503316480&z=&tn=baiduimagedetail&ipn=d&word=%E8%8B%B1%E5%9B%BDbus%E5%AE%98%E7%BD%91&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=-1&hd=&latest=&copyright=&cs=3302294045,2426532709&os=2290345771,540492404&simid=3360597861,62584341&pn=0&rn=1&di=29700&ln=1267&fr=&fmq=1588667085661_R&ic=&s=undefined&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=2&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fjynews.zjol.com.cn%2Fpic%2F0%2F10%2F08%2F56%2F10085659_995069.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined');">
     <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200&text=Logo" alt="">
   </header>
 
@@ -40,6 +40,9 @@ include('customer-system-header.html');
       <h1>Section Heading</h1>
       <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+    </div>
+    <div class="searching-box">
+      
     </div>
   </section>
 
@@ -76,7 +79,6 @@ include('customer-system-header.html');
 
 <?php
             
-mysqli_free_result($result);
 mysqli_close($connection);
 
 ?>      
