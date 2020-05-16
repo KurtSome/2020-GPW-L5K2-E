4b97d57a91b3f2b54c332753c8cdfebc41e2190c
KurtSome <45254780+KurtSome@users.noreply.github.com>
2020-05-16 23:32:28 +0800

    changed the db

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // 1. Open database connection
 $dbhost = "localhost";
-$dbuser = "he-sunzhengshu";
-$dbpass = "pp3qt700";
-$dbname = "2201713130116";
+$dbuser = "06ddb55aed56";
+$dbpass = "f4b1588cb2df1c28";
+$dbname = "l5k2e";
 
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
