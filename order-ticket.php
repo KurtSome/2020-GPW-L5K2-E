5fc5fc64115b989e91718fdc73a21c6c3bac89a4
KurtSome <45254780+KurtSome@users.noreply.github.com>
2020-05-17 17:23:51 +0800

    order-ticket.php

---
+++ order-ticket.php
@@ -0,0 +1,107 @@
+<?php
+// make db conection
+require('db.php');
+include('customer-system-header.html');
+
+$query = "SELECT bus_id, bus_started, bus_end, bus_comment ";
+$query .="FROM bus ";
+            
+    
+$result = mysqli_query($connection, $query);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Chengdu Bus Company</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/full-width-pics.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+
+
+  <!-- Header - set the background image for the header in the line below -->
+  <div class="container-fluid">
+
+          <!-- Page Heading -->
+        <div>
+        <h1 class="hszs1">Tickets</h1>
+        
+        
+        
+        </div>
+
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h4 class="">Bus Lines</h4>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Origin Station</th>
+                      <th>End Station</th>
+                      <th>Information</th>
+                      <th> </th>
+                    </tr>
+                  </thead>
+<?php    
+//show data        
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row["bus_id"] . "</td>";
+    echo "<td>" . $row["bus_started"] . "</td>";
+    echo "<td>" . $row["bus_end"] . "</td>";
+    echo "<td>" . $row["bus_comment"] . "</td>";
+    echo "<td><a href='customerdetail.php?id=" . $row["c_id"] . "'>Order</a></td>";
+    echo "</tr>";
+}
+          
+?>
+                  </table>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+
+<?php
+            
+mysqli_close($connection);
+
+?>      
