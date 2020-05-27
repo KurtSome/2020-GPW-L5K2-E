9b8b259cddbffa6ca916d9bfd980a75188899eec
KurtSome <45254780+KurtSome@users.noreply.github.com>
2020-05-27 21:06:23 +0800

    Didn't login customer system

---
+++ un-customer-system.php
@@ -0,0 +1,79 @@
+<?php
+// make db conection
+require('db.php');
+include('un-customer-system-header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Chengdu Bus Company</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/full-width-pics.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+
+
+  <!-- Header - set the background image for the header in the line below -->
+  <header class="py-5 bg-image-full" style="background-image: url('https://image.baidu.com/search/detail?ct=503316480&z=&tn=baiduimagedetail&ipn=d&word=%E8%8B%B1%E5%9B%BDbus%E5%AE%98%E7%BD%91&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=-1&hd=&latest=&copyright=&cs=3302294045,2426532709&os=2290345771,540492404&simid=3360597861,62584341&pn=0&rn=1&di=29700&ln=1267&fr=&fmq=1588667085661_R&ic=&s=undefined&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=2&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fjynews.zjol.com.cn%2Fpic%2F0%2F10%2F08%2F56%2F10085659_995069.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined');">
+    <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200&text=Logo" alt="">
+  </header>
+
+  <!-- Content section -->
+  <section class="py-5">
+    <div class="container">
+      <a href="order-ticket.php"><h1>Order Tickets</h1></a>
+      <p class="lead">Choosing a line that you want. <a href="order-ticket.php">Click Here</a></p>
+      <p>"Please check business tickets or economic tickets, and you can order a meal during the trip. Have good time.</p>
+    </div>
+    <div class="searching-box">
+      
+    </div>
+  </section>
+
+  <!-- Image Section - set the background image for the header in the line below -->
+  <section class="py-5 bg-image-full" style="background-image: url('https://unsplash.it/1900/1080?image=1081');">
+    <!-- Put anything you want here! There is just a spacer below for demo purposes! -->
+    <div style="height: 200px;"></div>
+  </section>
+
+  <!-- Content section -->
+  <section class="py-5">
+    <div class="container">
+      <h1>Section Heading</h1>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+    </div>
+  </section>
+
+  <!-- Footer -->
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+
+<?php
+            
+mysqli_close($connection);
+include('customer-system-footer.php');
+?>      
