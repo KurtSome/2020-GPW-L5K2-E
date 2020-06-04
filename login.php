fd28d8c880d11b82db01c0db7630089b048df6f5
KurtSome <45254780+KurtSome@users.noreply.github.com>
2020-06-04 12:22:24 +0800

    This is customer login.php

--- login.php
+++ login.php
@@ -1,79 +1,146 @@
 <?php
-
+// make db conection
 require('db.php');
 
-if (isset($_POST['login'])) {
-if (empty($_POST['username']) || empty($_POST['password'])) {
-    $error = "username or password is empty";
-} else {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if (isset($_POST['submit'])) {
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        $error = "username or password is empty";
+    } else { 
+        // Save username & password in a variable
+        $username = $_POST['c_identification'];
+        $password = $_POST['c_password'];
 
-    $query  = "SELECT username, password ";
-    $query .= "FROM manager ";
-    $query .= "WHERE username = '$username' AND password = '$password' ";
+        // 2. Prepare query
+        $query  = "SELECT c_identification, c_password "; 
+        $query .= "FROM customer ";
+        $query .= "WHERE c_identification = '$username' AND c_password = '$password' ";
 
-    $result = mysqli_query($connection, $query);
+        // 2. Execute query
+        $result = mysqli_query($connection, $query);
 
-    if (!$result) {
-        die("query is wrong");
-    }
+        if (!$result) {
+            die("query is wrong");
+        }
 
-    $row = mysqli_fetch_array($result);
-
-
-    $numrows=mysqli_num_rows($result);
-    if ($numrows == 1) {
-        session_start();
-
-        $_SESSION['login_user'] = $username;
-        header('location:index.php');
-    } else if($numrows == 0){
-        $error = 'username or password is wrong';
-    }
-
-
+        // Save data to $row
+        $row = mysqli_fetch_array($result);
+        
+        // Check how many answers did we get
+        $numrows=mysqli_num_rows($result);
+        if ($numrows == 1) {
+            // Start to use sessions
+            session_start();
+            
+            // Create session variables
+            $_SESSION['login_user'] = $username;
+            header('location: customer-system.php');
+            } else {
+            echo "Login failed!!!!!!!!!!!!!";
+        }
+        
+        // 4. free results
         mysqli_free_result($result);
     }
 }
 
+?>
 
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>CCL Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+    
+  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="container">
+            <div class="input-group">
+              <h1>Chengdu Consultancy Law</h1>
+            </div>
+            </div>
+          </form>
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                  </div>
+                <!--改输入栏格式-->
+                  <form class="user" action="login.php" method="post">
+                    <div class="form-group">
+                      <input type="username" name="username"  class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                    </div>
+                      <!--改成这样-->
+                    <input type="submit" name="submit" class="btn btn-primary btn-user btn-block" value="log">
+                    <hr>
+                  </form>
+                  <div class="text-center">
+                    <a class="small" href="register.php">Register</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
+
+<?php 
+// 5. close db connection
 mysqli_close($connection);
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="style1.css">
-</head>
-<body>
-<div class="main">
-    <div class="login">
-        <div class="logo">
-            CBC information management system
-            <?php
-            if (isset($error)){
-                echo "<p style='text-align: center; font-size: 20px;color: red;' >" . $error . "</p>";
-
-            }
-            ?>
-        </div>
-        <form action="login.php" method="POST">
-            <h1><label>Username:</label></h1>
-            <input type="text" name="username" placeholder="Please input your username">
-            <h1><label>Password:</label></h1>
-            <input type="password" name="password" placeholder="Please input your password">
-            <input type="submit" name="login" value="Login">
-        </form>
-    </div>
-</div>
-    <style>
-        body{
-    margin: 0;
-    padding: 0;
-    background-image: url(http://img4.99114.com/group10/M00/17/7F/rBADs1zeNvCAQdPPAAMae8eJLWE890.jpg)
-}</style>
-</body>
-</html>
